99311c3c258373ce5642ffa8916aef1a0ef67c59
matttrach <matt.trachier@suse.com>
2022-06-30 15:48:08 -0500

    adding drone and nix
    Signed-off-by: matttrach <matt.trachier@suse.com>

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A reliable testing environment";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            age
+            bashInteractive
+            bats
+            binutils
+            curl
+            file
+            gcc
+            gh
+            git
+            gnumake
+            go-tools
+            go_1_18
+            gopls
+            gotools
+            nettools
+            nixpkgs-fmt
+            openssl
+            shellcheck
+            shfmt
+            sops
+            terraform
+            terragrunt
+            wget
+            yq
+          ];
+        };
+      });
+}
